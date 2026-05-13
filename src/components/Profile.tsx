a9415379f2ed7a15fe23799007c3910ec8870a93
yojhanHuanca <yhuancayucra@gmail.com>
2026-05-13 17:24:39 -0500

    mi componente

---
+++ src/components/Profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return (
+        <header>
+            <h1>Yojhan Huanca Yucra</h1>
+            <h2>Frontend Developer</h2>
+        </header>
+    );
+};
+
+export default Profile;
